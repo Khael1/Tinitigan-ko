b80cd6efdeae612195a62168a99522cf7db120f4
Khael1 <122359723+Khael1@users.noreply.github.com>
2024-03-17 01:43:29 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,475 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What refers to the combination of physical objects used to communicate through physical objects, such as radio, television, computers, telephone, mobile phone, film, etc.",
+        answer: "D. Media",
+        options: [
+            "A. Communication",
+            "B. Information Literacy",
+            "C. Literacy",
+            "D. Media"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following information literacy skill demonstrates evaluation? The student is able to",
+        answer: "B. judge the result",
+        options: [
+            "A. locate sources.",
+            "B. judge the result",
+            "C. define the problem",
+            "D. able to organize information from multiple sources."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Paul Lives in a rural area where electricity is not available. He is very fond of hearing news when he was still living in the city. He has financial difficulty since he just lost his job.",
+        answer: "D. Use battery operated radio.",
+        options: [
+            "A. Buy a generator.",
+            "B. Buy television.",
+            "C. Install satellite television.",
+            "D. Use battery operated radio."
+        ]
+    },
+    {
+        numb: 4,
+        question: "How would you ensure the right security and privacy in your social media account?",
+        answer: "D. Set your social media account into private. Allowing information shared to be safe and secured",
+        options: [
+            "A. Do not divulge any information.",
+            "B. Keep friends that are known to you and do not accept unfamiliar people.",
+            "C. Set your social media account into invisible so that you are completely safe.",
+            "D. Set your social media account into private. Allowing information shared to be safe and secured"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Republic Act protects indigenous people and their rights?",
+        answer: "C. R.A. 8371",
+        options: [
+            "A. R.A. 9172",
+            "B. RLA. 8512",
+            "C. R.A. 8371",
+            "D. R.A. 1521"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following is NOT true about plagiarism?",
+        answer: "D. It is a form of harassment using electronic forms of contact",
+        options: [
+            "A. It includes other people's ideas in the paper.",
+            "B. Is uses someone's ideas in the paper without citing the source",
+            "C. It directly quotes a source instead of using one's own words",
+            "D. It is a form of harassment using electronic forms of contact"
+        ]    
+    },
+    {
+        numb: 7,
+        question: "If you want to introduce a product or services to the market, what form of media channel would you use?",
+        answer: "A. Advertising",
+        options: [
+            "A. Advertising",
+            "B. E-mail",
+            "C. Radio",
+            "D. Televiston"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Noli de Castro is one of the leading broadcast journalists in the country. He is always truthful, reliable, credible, and fair in manner. What do you think does Noli de Castro practice?",
+        answer: "B. Media Ethics",
+        options: [
+            "A. Patience",
+            "B. Media Ethics",
+            "C. Responsible Citizen",
+            "D. Trustworthy"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What visual media gives the appearance of a movement which can be a collection of graphics, footage and shading?",
+        answer: "B. motion",
+        options: [
+            "A. audio",
+            "B. motion",
+            "C. text",
+            "D. visual"
+        ]    
+    },
+    {
+        numb: 10,
+        question: "The video footage presented as evidence of the crime shows some clips were missed. What technical method needs to be checked?",
+        answer: "B. Smoothness of the video",
+        options: [
+            "A. Lighting coverage",
+            "B. Smoothness of the video",
+            "C. Scale and size consistency",
+            "D. Color and sound appropriateness"
+        ]    
+    },
+    {
+        numb: 11,
+        question: "As a competitive and honest Filipino learner, how can you help massive piracy in new media?",
+        answer: "B. Create a campaign advertisement against piracy.",
+        options: [
+            "A. Share free download site to your friends.",
+            "B. Create a campaign advertisement against piracy.",
+            "C. Ignore download sites that offers free moviesand music.",
+            "D. Avoid downloading movies and music from free download sites."
+        ]    
+    },
+    {
+        numb: 12,
+        question: "What is an original word of a piece of writing or a speech?",
+        answer: "C. Text",
+        options: [
+            "A. Denotation",
+            "B. Information",
+            "C. Text",
+            "D. Text information"
+        ]    
+    },
+    {
+        numb: 13,
+        question: "Which artwork functions as a source in addition to being displayed because of their significant values?",
+        answer: "B. Original Art",
+        options: [
+            "A. Moving Images",
+            "B. Original Art",
+            "C. Print",
+            "D. Photographs"
+        ]    
+    },
+    {
+        numb: 14,
+        question: "The design for visual message basically focuses on the considerations of using visual elements in accordance to the needs of the target audience. Which of the following consideration is NOT included?",
+        answer: "B. Visual literacy",
+        options: [
+            "A. Culture",
+            "B. Visual literacy",
+            "C. Educational ability",
+            "D. Language of the audience"
+        ]    
+    },
+    {
+        numb: 15,
+        question: "Ronie started to feel that his audience is not interested in listening to his presentation during his Science class. What should he do to catch the attention of his audience?",
+        answer: "A. Use visual information",
+        options: [
+            "A. Use visual information",
+            "B. Use text-based presentation",
+            "C. Apply state of the art presentation",
+            "D. Organize compiler during presentation"
+        ]    
+    },
+    {
+        numb: 16,
+        question: "Mr. Rudy Go, gave a visual presentation project to his students. He wanted the project to have a gloomy color appearance. What should his students apply to pass the project?",
+        answer: "D. murky colors in the creative visual material",
+        options: [
+            "A. both warm and light colors",
+            "B. flamboyant colors to attract audience",
+            "C. light colors in the creative visual material",
+            "D. murky colors in the creative visual material"
+        ]    
+    },
+    {
+        numb: 17,
+        question: "How would you ensure that your visual presentation is interesting and enticing?",
+        answer: "D. Apply the selection criteria as stated in making the visual presentation.",
+        options: [
+            "A. Use convincing materials.",
+            "B. Make the presentation as concise as possible.",
+            "C. Set the presentation as convincing as possible.",
+            "D. Apply the selection criteria as stated in making the visual presentation."
+        ]    
+    },
+    {
+        numb: 18,
+        question: "Which of the following is a form of internet audio broadcasting?",
+        answer: "B. Podcast",
+        options: [
+            "A. Blogging",
+            "B. Podcast",
+            "C. Radio",
+            "D. Television"
+        ]    
+    },
+    {
+        numb: 19,
+        question: "The following are principles of listening, EXCEPT:",
+        answer: "C. Feel confident and knows it all.",
+        options: [
+            "A Prepare yourself to listen",
+            "B. Listen for ideas not just words.",
+            "C. Feel confident and knows it all.",
+            "D. Listen to the tone of the speaker"
+        ]    
+    },
+    {
+        numb: 20,
+        question: "Which of the following is an act or process of changing the position or orientation of a certain body?",
+        answer: "C. Motion",
+        options: [
+            "A. Acting",
+            "B. Dreaming",
+            "C. Motion",
+            "D. Understanding"
+        ]    
+    },
+    {
+        numb: 21,
+        question: "Which of the following media dimensions is very useful to the development of listening skills?",
+        answer: "A. audio",
+        options: [
+            "A. audio",
+            "B. motion",
+            "C text",
+            "D. visual"
+        ]    
+    },
+    {
+        numb: 22,
+        question: "Adonis is using an audio file that is stored in a disk because he wants to listen it later. What do you think did Adonis used?",
+        answer: "D. Recording",
+        options: [
+            "A. Audio clip",
+            "B. Earcon",
+            "C. Footage",
+            "D. Recording"
+        ]    
+    },
+    {
+        numb: 23,
+        question: "What is the system of examining media materials to avoid causing offense, harm, or violence towards the public?",
+        answer: "A. Censorship",
+        options: [
+            "A. Censorship",
+            "B. Production",
+            "C. Mise-en-scene",
+            "D. Motion Information"
+        ]    
+    },
+    {
+        numb: 24,
+        question: "Which of the following is NOT a video editing software?",
+        answer: "B. Publisher",
+        options: [
+            "A. Avid",
+            "B. Publisher",
+            "C. Adobe Premier",
+            "D. Final Cut Pro"
+        ]    
+    },
+    {
+        numb: 25,
+        question: "Which of the following is a cinematography?",
+        answer: "D. Camera angles",
+        options: [
+            "A. Diegetic",
+            "B. Story based",
+            "C. Video magazine",
+            "D. Camera angles"
+        ]    
+    },
+    {
+        numb: 26,
+        question: "A group of students were not able to hit the desired motion media presentation criteria that their teacher wants, what can you suggest to creatively catch the interest of their teacher?",
+        answer: "D. Use the selection criteria of both visual and motion media since they are closely associated with one another.",
+        options: [
+            "A. The motion media must be comprehensive so that there should be a clear explanation on how videos should be presented.",
+            "B. Motion media should be catchy so that it has the advantage of getting immediate attention from its viewers.",
+            "C. It should be convincing as persuasive as possible.",
+            "D. Use the selection criteria of both visual and motion media since they are closely associated with one another."
+        ]    
+    },
+    {
+        numb: 27,
+        question: "If a person watches documentary about poverty, he or she might feel bad, not just because of pity. This statement narrates to what kind of motion information?",
+        answer: "B. Assessment of Ones Values",
+        options: [
+            "A. Theoretical Connections",
+            "B. Assessment of Ones Values",
+            "C. Showcasing of Complex Ideas",
+            "D. Learners and Culturally Relevant Events"
+        ]    
+    },
+    {
+        numb: 28,
+        question: "In a post conference of a motion media subject, one student got a comment from one of his professors: Your video is disorganized and lack of sensible scenes. What does the statement imply?",
+        answer: "C. The video needs a mise-en-scene and arranged it accordingly",
+        options: [
+            "A. The video is irrelevant",
+            "B. The video needs cinematography",
+            "C. The video needs a mise-en-scene and arranged it accordingly",
+            "D. The video needs a storyboard so that it can be understood and organized accordingly"
+        ]    
+    },
+    {
+        numb: 29,
+        question: "Why is mise-en-scene relevant in production?",
+        answer: "D. To avoid putting irrelevant things which do not contribute anything to the scene",
+        options: [
+            "A. To put interesting ideas into the film.",
+            "B. To highlight important things in the film.",
+            "C. To produce humorous scene that makes the audience alive.",
+            "D. To avoid putting irrelevant things which do not contribute anything to the scene"
+        ]    
+    },
+    {
+        numb: 30,
+        question: "How does censorship affect the audience?",
+        answer: "D. It allows audience to be informed and be warned on the sensitive materials in the video",
+        options: [
+            "A. It created delusion.",
+            "B. It makes the audience feel uneasy.",
+            "C. It makes the receiver misinformed.",
+            "D. It allows audience to be informed and be warned on the sensitive materials in the video"
+        ]    
+    },
+    {
+        numb: 31,
+        question: "How would you best promote the tourism of the country?",
+        answer: "A. Make a documentary film that exudes Philippine scenery.",
+        options: [
+            "A. Make a documentary film that exudes Philippine scenery.",
+            "B. Produce a text information that highlights best scenery.",
+            "C. Produce a film that talks about Philippine achievements.",
+            "D. Create a visual information providing all the best scenic spots in our country."
+        ]    
+    },
+    {
+        numb: 32,
+        question: "How is the principle of completeness being portrayed in making news reports?",
+        answer: "A. It answers the 5W and 1H questions.",
+        options: [
+            "A. It answers the 5W and 1H questions.",
+            "B. The news report has 4W and 1H questions",
+            "C. The news report has 3W and 1H questions",
+            "D. It does not require to answer any questions as long as it is full of details."
+        ]    
+    },
+    {
+        numb: 33,
+        question: "How many years for civilization to stay in the period of survival?",
+        answer: "D. Thousands",
+        options: [
+            "A. Centuries",
+            "B. Decades",
+            "C. Millions",
+            "D. Thousands"
+        ]    
+    },
+    {
+        numb: 34,
+        question: "What platform is used to make an online survey?",
+        answer: "B. Google forms",
+        options: [
+            "A. Chat room",
+            "B. Google forms",
+            "C. Facebook groups",
+            "D. Group conference"
+        ]    
+    },
+    {
+        numb: 35,
+        question: "Which of the following can be used to make a multimedia presentation?",
+        answer: "D. Power point",
+        options: [
+            "A. Paint",
+            "B. Publisher",
+            "C. Photoshop",
+            "D. Power point"
+        ]    
+    },
+    {
+        numb: 36,
+        question: "Which of the following is a disadvantage of multimedia?",
+        answer: "D. It is for those who can afford the equipment.",
+        options: [
+            "A. It utilizes a lot of senses.",
+            "B. It takes up a lot of storage space.",
+            "C. It uses modern electronic equipment.",
+            "D. It is for those who can afford the equipment."
+        ]    
+    },
+    {
+        numb: 37,
+        question: "Kyle wants to reach a vast number of people through his campaign advocacy. What do you think should he use to reach the global community?",
+        answer: "A. Internet",
+        options: [
+            "A. Internet",
+            "B. Newspaper",
+            "C. Radio",
+            "D. Television"
+        ]    
+    },
+    {
+        numb: 38,
+        question: "Diego wants to advertise his newly developed detergent. What should he used for his advertisement to be heard throughout the country? I. Internet, II. Radio, III. Television, IV. Mobile Phone",
+        answer: "D. I, II, III, IV",
+        options: [
+            "A. I",
+            "B. I and II",
+            "C. II and III",
+            "D. I, II, III, IV"
+        ]    
+    },
+    {
+        numb: 39,
+        question: "What do you think is the best multimedia presentation for someone who is on the go?",
+        answer: "A. Prezi",
+        options: [
+            "A. Prezi",
+            "B. Publisher",
+            "C. Adobe Premiere",
+            "D. Power point presentation"
+        ]    
+    },
+    {
+        numb: 40,
+        question: "How is a museum serves as a multimedia outlet?",
+        answer: "D. It offers a variety of information like text, visual and even motion media.",
+        options: [
+            "A. It uses state of the art equipment for viewing.",
+            "B. It offers past, present and future events",
+            "C. There are different sources that information can be available.",
+            "D. It offers a variety of information like text, visual and even motion media."
+        ]    
+    },
+    {
+        numb: 41,
+        question: "Why are multimedia personalities their mouth? careful about the words that come out of",
+        answer: "C. They should speak politely since they're role model.",
+        options: [
+            "A. They can speak whatever they like",
+            "B. Every single word will be used against them.",
+            "C. They should speak politely since they're role model.",
+            "D. They are influencer so whatever they speak will be emulated by many."
+        ]    
+    },
+    {
+        numb: 42,
+        question: "In a Media, which of the following are created to appeal to the emotion of the audience or to make the product appealing to the target audience?",
+        answer: "D. Media manipulations",
+        options: [
+            "A. Media films",
+            "B. Media conventions",
+            "C. Media endorsement",
+            "D. Media manipulations"
+        ]    
+    },
+    {
+        numb: 43,
+        question: "Which of the following is an act of causing people to do or believe something?",
+        answer: "C. Persuasion",
+        options: [
+            "A. Abomination",
+            "B. Conclusion",
+            "C. Persuasion",
+            "D. Solution"
+        ]    
+    },            
+];
